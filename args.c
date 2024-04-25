58030bbb9cf8118d15cc8868897c06640d15eddf
sergeyTabakaev <tabakaev.ca2003@gmail.com>
2024-04-25 12:00:00 +0000

    Dobavili programmu, obrabatyvayushyuyu parametry komandnoi stroki

---
+++ args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[], char *envp[]) {
+	printf("Kolichestvo parametrov, peredannykh programme %d\n", argc);
+	if (argc>1) {
+		for(int i=0 ; i<argc; i++) {
+			printf("Parametr %d: %s\n", i, argv[i]);
+		}
+	}
+	else{
+		int j=0;
+		while(envp[j]!=NULL){
+			printf("Parametr okruzheniya %d: %s\n", j, envp[j]);
+			j++;
+			}
+	}
+	return 0;
+}
